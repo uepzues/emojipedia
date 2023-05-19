6efe2e1149c562e6d153d43df3c797d6f8c8b820
zuesuep <kimsanz@gmail.com>
2023-05-19 13:32:18 +0800

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>
+          <a href="https://github.com/uepzues">uepzues</a>
+        </p>
+        <p>Copyright &copy; 2023</p>
+      </footer>
+    </>
+  );
+}
+export default Footer;
